63f52473b2605915fcee9fa13860697b35a14b45
Your Name <you@example.com>
2022-10-16 23:18:28 -0500

    modified app.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React User Interface Library.
+          React is a JavaScript Library.
         </p>
         <a
           className="App-link"
