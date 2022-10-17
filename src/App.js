21dc155043fe5f4147df81f3862dbdcf05fa22b1
Your Name <you@example.com>
2022-10-16 20:52:56 -0500

    modified app.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React Client Side Library.
+          React User Interface Library.
         </p>
         <a
           className="App-link"
